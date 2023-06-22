bc99b7d2e8463b74642fcb005b3041cd3bfe8d1b
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2023-06-22 18:42:56 -0400

    communication between customer and bank

--- bank.erl
+++ bank.erl
@@ -4,6 +4,10 @@
     withdraw_amount_from_bank(BankName, BankAmount, BankMap) ->
         receive
             {Sender, BankName} ->
-                io:fwrite("From Bank ID: ~w,~w,~w,~w\n\n", [BankName, BankAmount, self(), Sender])
+                io:fwrite("From Bank ID: ~w,~w,~w,~w\n\n", [BankName, BankAmount, self(), Sender]),
+                withdraw_amount_from_bank(BankName, BankAmount, BankMap);
+            {Sender, CustomerName, RandomAmountRequested} ->
+                io:fwrite("Bank approved ~w amount for Customer: ~w\n",[RandomAmountRequested, CustomerName]),
                 % io:fwrite("MM ~w",[whereis('bmo')])
+                withdraw_amount_from_bank(BankName, BankAmount, BankMap)
         end.
--- customer.erl
+++ customer.erl
@@ -3,6 +3,16 @@
 % -import(money,[start/1, customer_loop/4, bank_loop/3, initialize_banks/1, initialize_customers/2]).
 % -import(bank,[withdraw_amount_from_bank/3]).
 
+
+    % get_pid_from_bank_dict(BankDict, BankName) ->
+    %     case maps:is_key(BankName, BankDict) of
+    %         true ->
+    %             {ok, Pid} = maps:get(BankName, BankDict),
+    %             Pid;
+    %         false ->
+    %             %% Handle the case when the bank name is not found in the dictionary
+    %             undefined
+    %     end.
     request_amount_from_bank(CustomerName, CustomerRequestedAmount, CustomerMap, BankMap, BankDict) ->
         receive
             {Sender, CustomerName} ->
@@ -17,8 +27,16 @@
                 RandomAmountRequested = random:uniform(50) + 1,
                 % io:fwrite("From Customer ID: ~w,~w,~w,~w\n\n", [CustomerName, CustomerRequestedAmount, self(), RandomBank])
                 io:fwrite("? ~w requests a loan of ~w dollars(s) from the ~w bank\n", [CustomerName, RandomAmountRequested, RandomBank]),
-                
-                BankPId = erlang:whereis(RandomBank),
-                io:fwrite("MscccccsssM ~w",[BankDict]),
-                io:fwrite("Bank: ~w,~w\n", [BankPId,RandomBank])
+                case maps:get(RandomBank, BankDict) of
+                undefined ->
+                    io:fwrite("Bank not found in dictionary\n");
+                Pid ->
+                    io:fwrite("PID for ~w: ~w\n", [RandomBank, Pid]),
+                    % io:format("Stored Pid: ~p~n", [Pid]),
+                    % io:format("Is Pid: ~p~n", [is_pid(Pid)]),
+                    Pid ! {self(), CustomerName, RandomAmountRequested}
+                end
+
         end.
+
+    
