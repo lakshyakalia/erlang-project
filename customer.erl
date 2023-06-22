7953fc1dbd2fb36ebdf6aa58ca4ed84d8ddf50d0
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2023-06-22 16:36:50 -0400

    printing requested amount on screen

--- customer.erl
+++ customer.erl
@@ -4,5 +4,14 @@
     request_amount_from_bank(CustomerName, CustomerRequestedAmount, CustomerMap, BankMap) ->
         receive
             {Sender, CustomerName} ->
-                io:fwrite("From Customer ID: ~w,~w,~w,~w\n\n", [CustomerName, CustomerRequestedAmount, self(), Sender])
+                % RandomBank = get_random_key(BankMap),
+                % io:fwrite("hwhwh"),
+                random:seed(now()),
+                BankKeySet = maps:keys(BankMap),
+                RandomKeyIndex = random:uniform(length(BankKeySet)),   
+                RandomBank = lists:nth(RandomKeyIndex,maps:keys(BankMap)),
+                random:seed(now()),
+                RandomAmountRequested = random:uniform(50) + 1,
+                % io:fwrite("From Customer ID: ~w,~w,~w,~w\n\n", [CustomerName, CustomerRequestedAmount, self(), RandomBank])
+                io:fwrite("? ~w requests a loan of ~w dollars(s) from the ~w bank\n", [CustomerName, RandomAmountRequested, RandomBank])
         end.
